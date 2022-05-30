727a78a6a8c2edf20d4378b9a47bc623132a8bbb
JANVI <JANVI@JANHAVI>
2022-05-30 12:39:51 +0530

    Add project files.

---
+++ GitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Substract(500, 326);
+        }
+        static void Substract(int num1,int num2)
+        {
+            Console.WriteLine(num1-num2);
+        }
+    }
+}
